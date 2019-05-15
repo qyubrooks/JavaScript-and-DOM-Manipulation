da657f55e786e6b1e88d581f2299c1b747d536be
qyumichelle <qyubrooks@gmail.com>
2019-05-15 13:48:22 -0700

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# JavaScript-and-DOM-Manipulation
+# JavaScript and DOM Manipulation
+
+## Automatic Table and Date Search
+
+* Use a basic HTML web page 
+
+* Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to web page and then adds new rows of data for each UFO sighting.
+
+  * have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
+
+* Use a date form in my HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
+
+
+
---
+++ static/js/app.js
@@ -0,0 +1,43 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+// function build table
+function buildTable(data){
+    // First, clear out existing data
+    tbody.html("");
+    data.forEach((dataRow) =>{
+        //console.table(dataRow);
+        let row = tbody.append("tr");
+
+        //console.log(object.values(dataRow));
+        Object.values(dataRow).forEach((val) =>{
+            let cell = row.append("td")
+            cell.text(val);
+        });
+    })
+}
+
+// Event that call a function name it handLeClick
+function handleClick(){// function decoration
+    // prevent the form from refreshing the page
+    d3.event.preventDefault();
+
+    let date = d3.select('#datetime').property('value');
+    let filterData = tableData;
+
+    //Check to see if a date was entered and filter the data using that date;
+    if(date){
+        //Apply filter to the table data to only keep the
+        //rows where the datetime value matches the filter value
+        filterData = filterData.filter((row)=> row.datetime === date);
+
+    }
+    buildTable(filterData);
+}
+
+d3.selectAll('#filter-btn').on('click', handleClick);
+
+buildTable(tableData);
